3e79df84f577e3ad90f4d298d27f3e9466cc8d95
vikaskumar2500 <vikas.nits8084@gmail.com>
2024-07-26 03:35:16 +0530

    not done yet

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-const isPublicRoute = createRouteMatcher(["/", "/api/webhook(.*)"]);
+const isPublicRoute = createRouteMatcher(["/", "/api/webhook"]);
 
 export default clerkMiddleware((auth, req) => {
   if (auth().userId && isPublicRoute(req)) {
